dfbd79e1fe2cc5530f189b716e32a78f0dc52f7f
Kyriakos <kyriakos.bakirlis@gmail.com>
2023-06-26 18:29:44 +0200

    Fetching API key from config file.

--- WeatherApi/WeatherApiClient.cs
+++ WeatherApi/WeatherApiClient.cs
@@ -4,22 +4,23 @@ namespace WeatherApi;
 
 public interface IWeatherClient
 {
-    Task<WeatherData> GetRealTimeWeather(string city);
+    Task<WeatherData?> GetRealTimeWeather(string city);
 }
 
 public class WeatherApiClient : IWeatherClient
 {
     private readonly HttpClient _httpClient;
-    private readonly string apiKey = "";
+    private readonly string? _apiKey;
     
-    public WeatherApiClient(HttpClient httpClient)
+    public WeatherApiClient(HttpClient httpClient, IConfiguration config)
     {
         _httpClient = httpClient;
+        _apiKey = config.GetValue<string>("WeatherApi:ApiKey");
     }
 
-    public async Task<WeatherData> GetRealTimeWeather(string city)
+    public async Task<WeatherData?> GetRealTimeWeather(string city)
     {
         return await _httpClient.GetFromJsonAsync<WeatherData>(
-            $"current.json?key={apiKey}&q={city}&aqi=no");
+            $"current.json?key={_apiKey}&q={city}&aqi=no");
     }
 }
