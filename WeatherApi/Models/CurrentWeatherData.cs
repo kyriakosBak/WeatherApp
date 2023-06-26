f9c66415505e32932ffe343c258e6a8cbd407b8b
Kyriakos <kyriakos.bakirlis@gmail.com>
2023-06-26 20:45:23 +0200

    Fix for "current" part of weather reply not getting deserialized correctly

--- WeatherApi/Models/CurrentWeatherData.cs
+++ WeatherApi/Models/CurrentWeatherData.cs
@@ -2,29 +2,29 @@ namespace WeatherApi.Models;
 
 public record CurrentWeatherData
 {
-    public long LastUpdatedEpoch { get; init; }
-    public string LastUpdated { get; init; }
-    public double TempC { get; init; }
-    public double TempF { get; init; }
-    public int IsDay { get; init; }
-    public ConditionData Condition { get; init; }
-    public double WindMph { get; init; }
-    public double WindKph { get; init; }
-    public int WindDegree { get; init; }
-    public string WindDirection { get; init; }
-    public double PressureMb { get; init; }
-    public double PressureIn { get; init; }
-    public double PrecipitationMm { get; init; }
-    public double PrecipitationIn { get; init; }
-    public int Humidity { get; init; }
-    public int Cloud { get; init; }
-    public double FeelsLikeC { get; init; }
-    public double FeelsLikeF { get; init; }
-    public double VisibilityKm { get; init; }
-    public double VisibilityMiles { get; init; }
-    public double UvIndex { get; init; }
-    public double GustMph { get; init; }
-    public double GustKph { get; init; }
+    public long Last_Updated_Epoch { get; set; }
+    public string Last_Updated { get; set; }
+    public double Temp_C { get; set; }
+    public double Temp_F { get; set; }
+    public int Is_Day { get; set; }
+    public ConditionData Condition { get; set; }
+    public double Wind_Mph { get; set; }
+    public double Wind_Kph { get; set; }
+    public int Wind_Degree { get; set; }
+    public string Wind_Dir { get; set; }
+    public double Pressure_Mb { get; set; }
+    public double Pressure_In { get; set; }
+    public double Precip_Mm { get; set; }
+    public double Precip_In { get; set; }
+    public int Humidity { get; set; }
+    public int Cloud { get; set; }
+    public double Feelslike_C { get; set; }
+    public double Feelslike_F { get; set; }
+    public double Vis_Km { get; set; }
+    public double Vis_Miles { get; set; }
+    public double Uv { get; set; }
+    public double Gust_Mph { get; set; }
+    public double Gust_Kph { get; set; }
 }
 
 public record ConditionData
